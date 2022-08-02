a0083c61b232ec2f4a957bc6e1926543a48fb051
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-08-01 21:45:05 -0300

    testing rds connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'campus-backend'),
+      host: env('DATABASE_HOST', 'campus-database-jh-test.cjieupmz3alc.us-east-2.rds.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 33066),
+      database: env('DATABASE_NAME', 'campus-database-jh-test'),
       user: env('DATABASE_USERNAME', 'root'),
       password: env('DATABASE_PASSWORD', ''),
       ssl: env.bool('DATABASE_SSL', false),
