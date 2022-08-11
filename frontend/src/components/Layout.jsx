2a5c0e531d6fcbb1769c440a94b00f92da5826e4
Lautaro Vallejos <lautaro.vallejosk4m@gmail.com>
2022-08-11 17:51:39 -0300

    Minor Changes | modifying example directories

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from '../styles/Layout.module.css';
+
+export default function Layout(){
+    return (
+        <div className="main-container">
+            <h1>Campus Virtual JH</h1>
+            
+        </div>
+    )   
+}
